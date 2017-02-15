b8f3c04cd85996b5d8b3049cf034a076a462908d
phistoh <phis@mogobb.de>
2017-02-15 15:34:46 +0100

    Copy then compress instead of just compress
    Also Changed quotation marks

--- backup.lua
+++ backup.lua
@@ -14,7 +14,8 @@ end
 
 -- checks if a given path exists
 function path_exists(path)
-	if os.execute("cd \"" .. path .. "\" >nul 2>nul")  == nil then
+	-- >nul suppresses stdout ; 2>nul suppresses stderr
+	if os.execute('cd "' .. path .. '" >nul 2>nul')  == nil then
 		-- path doesn't exist
 		return false
 	else
@@ -24,71 +25,75 @@ function path_exists(path)
 end
 
 -- creates an compressed archive using 7z
--- parameters: path to 7z.exe, input file (or concatenated string of input files), name for output
-function create_archive(zip, infiles, outfile)	
+-- parameters: folder of files to compress, name for output
+function create_archive(infiles, outfile)	
 	-- create filename (including timestamp)
 	archive_file = outfile .. "-" .. os.time() .. ".7z"
 	
 	-- create archive
 	-- extra quote around the whole command because Windows-CMD removes the first and last quotation mark
-	os.execute("\"\"" .. zip .. "\" a " .. archive_file .. " " .. infiles .. "\"")
+	-- -mx9 specifies maximum compression
+	-- -sdel deletes files after including
+	-- -x!outfile-*.7z excludes all previously generated 7z files
+	os.execute('"7z.exe a ' .. infiles .. archive_file .. ' ' .. infiles .. '*" -mx9 -sdel -xr!' .. outfile .. '-*.7z')
 	
-	io.write("\n")
+	io.write('\n')
 end
 
 
 -----------
 -- "Main"
 -----------
+
 -- load table of files
-dofile("files.lua")
+dofile('files.lua')
 
 -- check if any argument was given
 if arg[1] == nil then
-	io.write("Usage: lua backup.lua name_of_table")
+	io.write('Usage: lua backup.lua name_of_table')
 	return
 end
 
 -- check if the given table exists
 if files[arg[1]] == nil then
-	io.write(arg[1] .. " is not defined in files.lua")
+	io.write(arg[1] .. ' is not defined in files.lua')
 	return
 end
 
 -- get copy of table and in/outpath
 local table_of_files = files[arg[1]]
-local inpath = table_of_files["inpath"]
-local outpath = table_of_files["outpath"]
+local inpath = table_of_files['inpath']
+local outpath = table_of_files['outpath']
 
 -- Check if the paths end with forward-slashed. If not -> add them
-if string.sub(inpath, string.len(inpath)) ~= "/" then
-	inpath = inpath .. "/"
+if string.sub(inpath, string.len(inpath)) ~= '\\' then
+	inpath = inpath .. '\\'
 end
-if string.sub(outpath, string.len(outpath)) ~= "/" then
-	outpath = outpath .. "/"
+if string.sub(outpath, string.len(outpath)) ~= '\\' then
+	outpath = outpath .. '\\'
 end
 
 -- check if paths exist
 -- 7z.exe
-if file_exists("7z.exe") == false then
-	io.write("7z.exe not found.\n")
+if file_exists('7z.exe') == false then
+	io.write('7z.exe not found.\n')
 	return false
 end
 
 -- input
 if path_exists(inpath) == false then
-	io.write("The path \""..inpath.."\" is not valid\n")
+	io.write('The path "'..inpath..'" is not valid\n')
 	return
 end
 
 -- output
 if path_exists(outpath) == false then
-	io.write("The path \""..outpath.."\" does not exist.\nShould it be created? (y/n) ")
+	io.write('The path "'..outpath..'" does not exist.\nShould it be created? (y/n) ')
 	-- get user input
 	local create_path = io.read()
-	if create_path == "y" then
+	if create_path == 'y' then
 		-- create folder
-		os.execute("md \"" .. outpath .. "\"")
+		os.execute('md "' .. outpath .. '"')
 	else
 		-- exit
 		return
@@ -96,22 +101,24 @@ if path_exists(outpath) == false then
 end
 
 -- start timer
-io.write("Copying started...\n")
+io.write('Copying started...\n')
 local start = os.time()
 
 -- init empty string and number
-input_string = ""
+input_string = ''
 file_count = 0
 
 -- iterate over table of inputfiles
-for k,current_file in pairs(table_of_files["filenames"]) do
+for k,current_file in pairs(table_of_files['filenames']) do
 	-- check if file exists
 	if file_exists(inpath .. current_file) == true then
-		-- concatenate string and increase file_count
-		input_string = input_string .. " \"" .. inpath .. current_file .. "\""
+		--increase file_count and copy file
 		file_count = file_count + 1
+		-- >nul suppresses stdout ; 2>nul suppresses stderr
+		os.execute('copy "' .. inpath .. current_file .. '" "' .. outpath .. '" >nul 2>nul')
+		io.write(current_file .. ' copied.\n')
 	else
-		io.write(current_file .. " not found. Will be skipped.\n")
+		io.write(current_file .. ' not found. Will be skipped.\n')
 	end
 end
 
@@ -120,9 +127,9 @@ input_string = string.sub(input_string, 2)
 
 -- create the archive (if there were files to compress)
 if file_count > 0 then
-	create_archive("7z.exe ", input_string, outpath .. arg[1])
+	create_archive(outpath, arg[1])
 end
 
 -- stop timer
 local elapsedTime = os.difftime(os.time(), start)
-io.write(string.format("%d File(s) copied in %.1f seconds.", file_count, elapsedTime))
+io.write(string.format('%d File(s) copied in %.1f seconds.', file_count, elapsedTime))
--- files.lua
+++ files.lua
@@ -1,11 +1,23 @@
 files = {}
 
--- use / to separate folders
+-- use \\ to separate folders
 files["dummy"] = {
-	inpath = "d:/um/my/",
-	outpath = "d:/ile/path/",
+	inpath = "d:\\um\\my\\",
+	outpath = "d:\\ile\\path\\",
 	filenames = {
 		"file1.xyz",
 		"another_fi.le"
+	}
+}
+
+files["a"] = {
+	inpath = "D:\\Users\\Gobbert\\Desktop\\a",
+	outpath = "D:\\Users\\Gobbert\\Desktop\\b",
+	filenames = {
+		"a.txt",
+		"b.txt",
+		"b.bmp",
+		"another_fi.le",
+		"minecreep.avi"
 	}
 }
